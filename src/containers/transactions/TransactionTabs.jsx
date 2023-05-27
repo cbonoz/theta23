eac30e9d407f320666f4ef29bcc2a172e84ce94a
Rikako Hatoya <15hatoyar@gmail.com>
2023-05-27 16:58:15 -0700

    Updated pagination on transactions page.

--- src/containers/transactions/TransactionTabs.jsx
+++ src/containers/transactions/TransactionTabs.jsx
@@ -1,26 +1,34 @@
 import {React, useState} from 'react'
-import { Box, Tabs, Tab } from '@mui/material';
-import { TabPanel } from '@mui/lab';
+import { Box, Tab } from '@mui/material';
+import { TabPanel, TabList, TabContext } from '@mui/lab';
+import { Analytics, RegisteredPets } from '..';
 
 const TransactionTabs = () => {
-  const [tabValue, setTabValue] = useState(0)
-  const handleChange = (value) => {
+  const [tabValue, setTabValue] = useState("0")
+  const handleChange = (e, value) => {
     setTabValue(value)
   }
   return (
     <div>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={tabValue} onChange={(e, value) => handleChange(value)}>
-          <Tab value={0} label="Registered Pets" />
-          <Tab value={1} label="Analytics" />
-        </Tabs>
-      </Box>
-      {/* <TabPanel value={tabValue} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={tabValue} index={1}>
-        Item Two
-      </TabPanel> */}
+      <Box>
+          <TabContext value={tabValue}>
+            <Box sx={{ borderBottom: 1, 
+                borderColor: "divider" }}>
+              <TabList
+                onChange={handleChange}
+              >
+                <Tab label="Registered Pets" value="0" />
+                <Tab label="Analytics" value="1" />
+              </TabList>
+            </Box>
+            <TabPanel value="0">
+              <RegisteredPets />
+            </TabPanel>
+            <TabPanel value="1">
+              <Analytics />
+            </TabPanel>
+          </TabContext>
+        </Box>
     </div>
   )
 }
